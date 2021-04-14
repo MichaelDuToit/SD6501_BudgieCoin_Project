1d89006ca009b83fa49038a2fadc5c3cc3c6a7d4
Michael Du Toit <michael.dtoit@gmail.com>
2021-04-14 17:14:32 +1200

    Implemented Biometric Authentication on the LoginActivity

--- app/src/main/java/com/example/sd6501_budgiecoin_project/LoginActivity.java
+++ app/src/main/java/com/example/sd6501_budgiecoin_project/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.sd6501_budgiecoin_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Build;
@@ -10,29 +14,79 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.concurrent.Executor;
+
+
 public class LoginActivity extends AppCompatActivity {
 
     EditText userInput, pinInput;
     TextView errorMsg;
     String user = "admin", pin = "4321";
-
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo biometricPromptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Assign layout fields to variables
         userInput = findViewById(R.id.loginUsername);
         pinInput = findViewById(R.id.loginPin);
         errorMsg = findViewById(R.id.loginError);
+
+        // Configure Biometric Login Prompts and methods
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(LoginActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            // Override onAuthenticationError method
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+            // Override onAuthenticationSucceeded method. If Authentication succeeds, invoke
+            // goToMainActivity method to change to home screen.
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                if(BiometricManager.BIOMETRIC_SUCCESS == 0){
+                    goToMainActivity();
+                }
+            }
+            // Override on onAuthenticationFailed method
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        // Use to BiometricPrompt builder to create the Android Biometric login prompt
+        biometricPromptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Login")            // These strings had to be hardcoded as
+                .setSubtitle("Login with Biometrics")   // running the app with R.string references
+                .setNegativeButtonText("Cancel")        // kept failing.
+                .build();
     }
 
-    public void authenticateUser(){
-        if(userInput.getText().toString().equals(user)){
-            if(pinInput.getText().toString().equals(pin)){
-                startActivity(new Intent(this, MainActivity.class));
-            }
-            else {
+    // If the user clicks the Login with Pin btn, call the pinLoginAuthentication method.
+    public void pinLoginBtn(View v){
+        pinLoginAuthentication();
+    }
+
+    // If the user clicks the Login with Biometrics, call the biometricPrompt to handle
+    // the authentication.
+    public void biometricLoginBtn(View v){
+        biometricPrompt.authenticate(biometricPromptInfo);
+    }
+
+    // If the user attempts to login using username and password, check their details
+    // and authenticate them.
+    public void pinLoginAuthentication(){
+        if (userInput.getText().toString().equals(user)) {
+            if (pinInput.getText().toString().equals(pin)) {
+                // If both the username and password are correct, call the goToMainActivity method.
+                goToMainActivity();
+            } else {
                 errorMsg.setText(R.string.loginPinIncorrect);
             }
         } else {
@@ -40,10 +94,22 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void loginBtn(View v){
-        authenticateUser();
+    // General purpose method to hold the intent call to go to the MainActivity class.
+    public void goToMainActivity(){
+        startActivity(new Intent(this, MainActivity.class));
     }
 
+    // If the user clicks the Enter tick key on the keyboard, attempt to
+    // login using the Pin Login authentication.
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_ENTER){
+            pinLoginAuthentication();
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    // Override the onBackPressed method to close the application.
     @Override
     public void onBackPressed() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
@@ -51,15 +117,5 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
-    }
-
-    // If the user clicks the enter key on the keyboard, attempt to login
-
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_ENTER){
-            authenticateUser();
-        }
-        return super.onKeyUp(keyCode, event);
     }
 }
